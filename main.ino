0a626858d0d284caaaad6d63cf0b4097acad21da
Dragan Vlad <vlad.dragan96@icloud.com>
2022-02-19 13:40:42 +0200

    Implemented closeGate & openGate functions

--- main.ino
+++ main.ino
@@ -3,6 +3,8 @@
 #include "virtualTimers.h"
 #include "actuatorCommands.h"
 
+#define C_GATE_PIN_NB     (13)
+
 /* =================================================
 *  Static variables definition
 *  ================================================= */
@@ -14,6 +16,7 @@ static t_int32u       s_waitTime = 30 * C_SECONDS_TO_US;
 *  ================================================= */
 static t_error sendCloseGateCommand(void);
 static t_error sendOpenGateCommand(void);
+static t_error sendImpulseToPin(p_pinNb)
 
 void setup() {
     plugActuatorCommands(&sendOpenGateCommand, &sendCloseGateCommand);
@@ -23,6 +26,9 @@ void setup() {
 
     virtualTimerInit(s_lineSensorVirtualTimer, s_waitTime);
     passTimeVariableToBackend(&s_waitTime);
+
+    /* PIN setup */
+    pinMode(C_GATE_PIN_NB, OUTPUT);
 }
 
 void loop() {
@@ -41,12 +47,30 @@ void loop() {
 *  ================================================= */
 static t_error sendCloseGateCommand(void) {
     t_error l_error = E_NOK;
-
+    l_error = sendImpulseToPin(C_GATE_PIN_NB)
     return l_error;
 }
 
 static t_error sendOpeneGateCommand(void) {
     t_error l_error = E_NOK;
+    l_error = sendImpulseToPin(C_GATE_PIN_NB)
+    return l_error;
+}
+
+static t_error sendImpulseToPin(p_pinNb) {
+    t_error l_error = E_NOK;
+    /* Read the current pin state */
+    t_bool l_currentPinState = analogRead(p_pinNb);
+
+    digitalWrite(p_pinNb, !l_currentPinState);
+    delay(1 * C_SECONDS_TO_US);
+    digitalWrite(p_pinNb, l_currentPinState);
+    delay(1 * C_SECONDS_TO_US);
+
+    /* Check if the pin has returned to it's original state */
+    if(l_currentPinState == analogRead(p_pinNb)) {
+        l_error = E_OK;
+    }
 
     return l_error;
 }
